97b62590e600472bfa34b4958e72dc125c2027a1
Minihove06 <aruibra030@g.educaand.es>
2025-02-11 14:05:37 +0100

    Commit inicial con el enunciado, las dependencias y los tests.
    mi nombre
    y cosicas de cliente

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/dominio/Cliente.java
@@ -0,0 +1,33 @@
+package org.iesalandalus.programacion.tallermecanico.modelo.dominio;
+
+public class Cliente {
+    private static final String er_nombre = "pepe";
+    private static final String er_dni = "12345678C";
+    private static final String er_telefono = "660123456";
+    private String nombre;
+    private String dni;
+    private String telefono;
+
+    public Cliente(String nombre, String dni, String telefono){
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Cliente setNombre(String nombre) {
+        this.nombre = nombre;
+        return this;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public Cliente setDni(String dni) {
+        this.dni = dni;
+        return this;
+    }
+    private comprobarLetraDni(dni string:Cliente)
+}
