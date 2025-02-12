40ed6ad586312bb17cd00d3dd6dd3132e9498d6e
Minihove06 <aruibra030@g.educaand.es>
2025-02-12 09:31:45 +0100

    si
    se añade exception

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/TallerMecanicoException.java
@@ -0,0 +1,7 @@
+package org.iesalandalus.programacion.tallermecanico.modelo;
+
+public class TallerMecanicoException extends RuntimeException {
+    public TallerMecanicoException(String message) {
+        super(message);
+    }
+}
