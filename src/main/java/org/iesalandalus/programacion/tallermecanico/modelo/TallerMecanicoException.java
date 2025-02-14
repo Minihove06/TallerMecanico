d48935aac263ec735801b7648d912c3fd576365d
Minihove06 <aruibra030@g.educaand.es>
2025-02-14 13:24:53 +0100

    Commit inicial con el enunciado, las dependencias y los tests.
    mi nombre
    esta cosa se supone q furrula
    
    los testeos en casa se miran

--- src/main/java/org/iesalandalus/programacion/tallermecanico/modelo/TallerMecanicoException.java
+++
@@ -1,7 +0,0 @@
-package org.iesalandalus.programacion.tallermecanico.modelo;
-
-public class TallerMecanicoException extends RuntimeException {
-    public TallerMecanicoException(String message) {
-        super(message);
-    }
-}
